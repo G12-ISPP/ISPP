ac554fd57f19911079ad72a51aa314ff771b2822
diegarlin <garcialinaresdiego@gmail.com>
2024-02-25 11:35:37 +0100

    fix: traducción de validaciones

--- backend/users/views.py
+++ backend/users/views.py
@@ -5,6 +5,11 @@ from rest_framework.exceptions import ValidationError
 from .models import CustomUser
 from rest_framework.decorators import action
 from users.serializer import UserSerializer
+from django.utils.translation import gettext_lazy as _
+from functools import wraps
+from django.utils.translation import activate
+from django.conf import settings
+
 
 class UsersView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
@@ -16,19 +21,29 @@ class UsersView(viewsets.ModelViewSet):
         user = self.get_object()
         serializer = self.get_serializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
-      
+    
+def translate(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        activate(settings.LANGUAGE_CODE)
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
 class UserCreateAPIView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
+    @translate
     def post(self, request, *args, **kwargs):
         try:
             return super().post(request, *args, **kwargs)
         except ValidationError as e:
             error_detail = dict(e.detail)
-            return Response(error_detail, status=status.HTTP_400_BAD_REQUEST)
+            translated_errors = {key: [_(value[0])] for key, value in error_detail.items()}
+            print(translated_errors)
+            return Response(translated_errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             error_message = str(e)
             print(error_message)
-            return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': _('An error occurred.')}, status=status.HTTP_400_BAD_REQUEST)
