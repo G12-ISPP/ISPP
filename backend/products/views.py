c1f835d9ceefd1f29f66e434b797abe7b1ab7486
PabloMera6 <pabmergom@alum.us.es>
2024-02-25 17:51:08 +0100

    feat nuevos cambios
    añadida lógica, faltan métodos de develop

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,18 +1,77 @@
-from django.shortcuts import render
-
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .forms import ProductForm
+from django.views.decorators.csrf import csrf_exempt
+from .models import Product
+from django.contrib.auth.decorators import login_required
 
 @api_view(['POST'])
+@csrf_exempt
+@login_required
 def add_product(request):
     if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.seller = request.user
-            product.save()
-            return JsonResponse({'message': 'Product added successfully'})
-        else:
-            return Response(form.errors, status=400)
+        product_type = request.data.get('product_type')
+        price = request.data.get('price')
+        name = request.data.get('name')
+        description = request.data.get('description')
+        stock_quantity = request.data.get('stock_quantity')
+        image = request.FILES.get('file')
+
+        # Verificar que todos los campos requeridos estén presentes
+        if not all([product_type, price, name, description, image]):
+            return JsonResponse({'error': 'Todos los campos son obligatorios'}, status=400)
+
+        # Verificar el tipo de producto
+        if product_type not in dict(Product.TYPE_CHOICES):
+            return JsonResponse({'error': 'Tipo de producto no válido'}, status=400)
+
+        # Validar el precio
+        try:
+            price = float(price)
+            if price <= 0:
+                return JsonResponse({'error': 'El precio debe ser mayor que cero'}, status=400)
+        except ValueError:
+            return JsonResponse({'error': 'El precio debe ser un número válido'}, status=400)
+
+        # Validar la cantidad de stock
+        try:
+            stock_quantity = int(stock_quantity)
+            if stock_quantity < 0:
+                return JsonResponse({'error': 'La cantidad de stock no puede ser negativa'}, status=400)
+        except ValueError:
+            return JsonResponse({'error': 'La cantidad de stock debe ser un número entero válido'}, status=400)
+
+        # Guardar el producto en la base de datos
+        product = Product(
+            product_type=product_type,
+            price=price,
+            name=name,
+            description=description,
+            stock_quantity=stock_quantity,
+            seller=request.user 
+        )
+        product.save()
+
+        image = request.FILES.get('file')
+        image_name = f'{product.id}.jpg'  # Generar el nombre de la imagen basado en el ID del producto
+        product.imageRoute = image_name
+        product.save()
+
+        return JsonResponse({'message': 'Producto añadido correctamente'}, status=201)
+
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+from django.http import JsonResponse
+from django.core.files.storage import FileSystemStorage
+
+@api_view(['POST'])
+@csrf_exempt
+def upload_image(request):
+    if request.method == 'POST':
+        image = request.FILES.get('file')
+        if image:
+            fs = FileSystemStorage(location='/public')
+            filename = fs.save(image.name, image)
+            uploaded_file_url = fs.url(filename)
+            return JsonResponse({'image_url': uploaded_file_url})
+        return JsonResponse({'error': 'No se seleccionó ninguna imagen'}, status=400)
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -12,6 +12,7 @@ class Product extends Component {
       stockQuantity: 1,
       productType: 'P',
       imagePreview: '../../public/design_hucha_cerdo.jpg',
+      errors: {}
     };
 
     this.handleFileChange = this.handleFileChange.bind(this);
@@ -32,35 +33,82 @@ class Product extends Component {
     }
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-   
-    const formData = new FormData();
-    formData.append('file', this.state.file);
-    formData.append('name', this.state.name);
-    formData.append('description', this.state.description);
-    formData.append('price', this.state.price);
-    formData.append('stock_quantity', this.state.stockQuantity);
-    formData.append('product_type', this.state.productType);
-   
-    fetch('http://127.0.0.1:8000/products/add-product', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => {
-      if (response.ok) {
-        alert('Producto añadido correctamente');
-      } else {
-        alert('Error al añadir el producto');
-      }
-    })
-    .catch(error => {
-      console.error('Error al enviar el formulario:', error);
-      alert('Error al enviar el formulario');
-    });
+  validateForm() {
+    const { name, description, price, stockQuantity } = this.state;
+    const errors = {};
+
+    if (!name.trim()) {
+      errors.name = 'El nombre es obligatorio';
+    }
+
+    if (!description.trim()) {
+      errors.description = 'La descripción es obligatoria';
+    }
+
+    if (price <= 0) {
+      errors.price = 'El precio debe ser mayor que cero';
+    }
+
+    if (stockQuantity < 0) {
+      errors.stockQuantity = 'La cantidad de stock no puede ser negativa';
+    }
+
+    this.setState({ errors });
+
+    return Object.keys(errors).length === 0;
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+     
+    if (this.validateForm()) {
+      const formData = new FormData();
+      formData.append('file', this.state.file);
+      formData.append('name', this.state.name);
+      formData.append('description', this.state.description);
+      formData.append('price', this.state.price);
+      formData.append('stock_quantity', this.state.stockQuantity);
+      formData.append('product_type', this.state.productType);
+     
+      fetch('http://127.0.0.1:8000/products/add-product', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => {
+        if (response.ok) {
+          // Subir la imagen al frontend
+          const imageFormData = new FormData();
+          imageFormData.append('file', this.state.file);
+          return fetch('http://127.0.0.1:8000/upload-image', {
+            method: 'POST',
+            body: imageFormData
+          });
+        } else {
+          throw new Error('Error al añadir el producto');
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.image_url) {
+          // Actualizar el estado con la URL de la imagen
+          this.setState({ imagePreview: data.image_url });
+          alert('Producto añadido correctamente');
+        } else {
+          throw new Error('Error al subir la imagen al frontend');
+        }
+      })
+      .catch(error => {
+        console.error('Error al enviar el formulario:', error);
+        alert('Error al enviar el formulario');
+      });
+    } else {
+      alert('Por favor, corrija los errores en el formulario');
+    }
+  }
+  
+
   render() {
+    const { errors } = this.state;
     return (
       <>
         <h1 className='title'>Mi Producto</h1>
@@ -75,19 +123,23 @@ class Product extends Component {
               </label>
               <div className='file-select'>
                 <input type='file' id='file' name='file' accept='image/*' onChange={this.handleFileChange} />
+                {errors.file && <div className="error">{errors.file}</div>}
               </div>
             </div>
             <div className='form-group'>
               <label className='name'>Nombre</label>
               <input type='text' id='name' name='name' value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
+              {errors.name && <div className="error">{errors.name}</div>}
             </div>
             <div className='form-group'>
               <label className='description'>Descripción</label>
               <textarea id='description' name='description' value={this.state.description} onChange={(e) => this.setState({ description: e.target.value })} />
+              {errors.description && <div className="error">{errors.description}</div>}
             </div>
             <div className='form-group'>
               <label className='price'>Precio</label>
               <input type='number' id='price' name='price' value={this.state.price} onChange={(e) => this.setState({ price: e.target.value })} />
+              {errors.price && <div className="error">{errors.price}</div>}
             </div>
             <div className='form-group'>
             <label className='product-type'>Tipo</label>
@@ -99,13 +151,14 @@ class Product extends Component {
                     <option value='I'>Pieza</option>
                 </select>
             </div>
-        </div>
-        {this.state.productType !== 'D' && (
-            <div className='form-group'>
-                <label className='stock-quantity'>Cantidad</label>
-                <input type='number' id='stock-quantity' name='stock-quantity' value={this.state.stockQuantity} onChange={(e) => this.setState({ stockQuantity: e.target.value })} />
             </div>
-        )}
+            {this.state.productType !== 'D' && (
+                <div className='form-group'>
+                    <label className='stock-quantity'>Cantidad</label>
+                    <input type='number' id='stock-quantity' name='stock-quantity' value={this.state.stockQuantity} onChange={(e) => this.setState({ stockQuantity: e.target.value })} />
+                    {errors.stockQuantity && <div className="error">{errors.stockQuantity}</div>}
+                </div>
+            )}
           </form>
         </div>
         <button className='add-product-button' type='button' onClick={this.handleSubmit}>Añadir Producto</button>
