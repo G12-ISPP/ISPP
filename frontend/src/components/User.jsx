d488fbe029725aad7188179be062385390ce920c
David Reyes <dreyes@inoutlabs.es>
2024-02-29 23:12:03 +0100

    Fix/Resolución de conflicto

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -28,7 +28,6 @@ const UserDetail = () => {
 
     fetchUserData();
   }, []);
-  
 
   const handleChatClick = async () => {
     const currentUserID = localStorage.getItem('userId');
@@ -53,51 +52,52 @@ const UserDetail = () => {
       navigate(`/chat/${data.chatroomID}`);
     } catch (error) {
       console.error('Error:', error);
-
-      return (
-        <>
-            <div className="section-title-container">
-                <Text type={TEXT_TYPES.TITLE_BOLD} text='Detalles de usuario' />
-            </div>
-
-            {user.is_designer || user.is_printer ? (
-                <div className="main-info-container">
-                    <h2 className='title'>Rol de {user.first_name} {user.last_name}</h2>
-                    <div className="user-role-container">
-                        <h3 className="user-role">{user.is_designer === true ? 'Diseñador ' : null}
-                            {user.is_printer === true ? ' Impresor' : null}</h3>
-                    </div>
-                </div>
-            ) : null}
-                <div className="main">
-                    <div className="user-img-container">
-                        <img className='img' src='/images/avatar.svg' alt={user.username} />
-                    </div>
-
-                    <div className="profile-summary">
-                        <div>
-                            <h2 className="title-detalle">{user.first_name} {user.last_name}</h2>
-
-                            <h3 className="title-detalle">Localización:</h3>
-                            <p>{user.address}</p>
-                            <p>{user.city}</p>
-                            <p>{user.postal_code}</p>
-
-                            <h3 className="title-detalle">Contacto:</h3>
-                            <p>{user.email}</p>
-                        </div>
-                        <div className="chat">
-                        <Button onClick={handleChatClick} type={BUTTON_TYPES.TRANSPARENT} text='Chat' />
-                    </div>  
-                    </div>
-
-                    
-                </div>
-
-            </>
-        );
     }
-  }
   };
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <div className="section-title-container">
+          <Text type={TEXT_TYPES.TITLE_BOLD} text='Detalles de usuario' />
+      </div>
+
+      {user.is_designer || user.is_printer ? (
+          <div className="main-info-container">
+              <h2 className='title'>Rol de {user.first_name} {user.last_name}</h2>
+              <div className="user-role-container">
+                  <h3 className="user-role">{user.is_designer === true ? 'Diseñador ' : null}
+                      {user.is_printer === true ? ' Impresor' : null}</h3>
+              </div>
+          </div>
+      ) : null}
+
+      <div className="main">
+          <div className="user-img-container">
+              <img className='img' src='/images/avatar.svg' alt={user.username} />
+          </div>
+
+          <div className="profile-summary">
+              <div>
+                  <h2 className="title-detalle">{user.first_name} {user.last_name}</h2>
+
+                  <h3 className="title-detalle">Localización:</h3>
+                  <p>{user.address}</p>
+                  <p>{user.city}</p>
+                  <p>{user.postal_code}</p>
+
+                  <h3 className="title-detalle">Contacto:</h3>
+                  <p>{user.email}</p>
+              </div>
+              <div className="chat">
+                <Button type={BUTTON_TYPES.TRANSPARENT} text='Chat' onClick={handleChatClick} />
+              </div>  
+          </div>
+      </div>
+    </>
+  );
+};
 
 export default UserDetail;
