3be19ebae62c19186c6b94a6d205282c1311357a
carloosbaquero <carlos.baqueville@gmail.com>
2024-02-29 13:41:25 +0100

    test inicio de sesion correcto en backend
    Se ha probado que el inicio de sesión se realiza cuando se mandan los datos correctos en el backend

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import CustomUser
 
-# Create your tests here.
+class LoginTestCase(TestCase):
+
+    def setUp(self):
+        CustomUser.objects.create_user(username='UserTest123', email='testemail@gmail.com', password='UserPass123', address='TestAddress', postal_code=12345, city='Sevilla')
+        super().setUp()
+    
+    def tearDown(self):
+        super().tearDown()
+
+    def test_login(self):
+        data = {'username': 'UserTest123', 'password': 'UserPass123'}
+        response = self.client.post('/users/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
