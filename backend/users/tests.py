5b6a354b8b8895c721cf74d3c00497c74a100955
carloosbaquero <carlos.baqueville@gmail.com>
2024-02-29 11:09:00 +0100

    test registro sin contraseña en backend
    Se ha realizado un test para comprobar que si al realizar un registro de usuario no se le pasa el parámetro de contraseña, se produce un error y no se crea el usuario

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import CustomUser
 
-# Create your tests here.
+class UsersTestCase(TestCase):
+    
+    def setUp(self):
+        super().setUp()
+    
+    def tearDown(self):
+        super().tearDown()
+    
+    def test_register_no_password(self):
+        data = {'username': 'UserTest123', 'name': 'NameTest', 'address': 'AddressTest', 'postal_code': '06228', 'city': 'Sevilla'}
+        response = self.client.post('/users/api/v1/users/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+        u = CustomUser.objects.filter(username='UserTest123').exists()
+        self.assertFalse(u)
