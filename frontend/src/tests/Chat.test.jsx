177ca99f1450beb1cd83327fd640d5e9a8eb3fb1
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-16 19:17:41 +0100

    test: Added integration testing

--- frontend/src/tests/Chat.test.jsx
+++ frontend/src/tests/Chat.test.jsx
@@ -4,8 +4,9 @@ import {ChatPage} from '../pages/ChatPage';
 import {BrowserRouter as Router} from 'react-router-dom';
 import ChatComponent from '../components/Chat/ChatComponent';
 import '@testing-library/jest-dom';
-import {getUser, register} from "../api/users.api";
-import {createChatRoom} from "../api/chat.api.jsx";
+import {getUser, login, register} from "../api/users.api";
+import {createChatRoom, getMessages} from "../api/chat.api.jsx";
+import { act } from 'react-dom/test-utils';
 
 const backend = JSON.stringify(import.meta.env.VITE_APP_BACKEND).replace(/"/g, '');
 
@@ -89,8 +90,6 @@ describe('Test unitarios de Chat', () => {
 });
 
 
-
-
 describe('Test de integración de Chat', () => {
 
     let user1 = undefined;
@@ -99,59 +98,145 @@ describe('Test de integración de Chat', () => {
     let chat_room = undefined;
 
     async function tearUp() {
+        localStorage.removeItem('token')
+
         user1 = await (await getUser(12)).json()
         user2 = await (await getUser(13)).json()
 
-        console.log(user1)
 
-        token = await getToken(user1.username, user1.password);
-        console.log('Token: ', token.access);
+        token = (await (await login('test1Frontend', 'test1Frontend')).json()).token;
 
-        localStorage.setItem('token', token.access);
+        localStorage.setItem('token', token);
 
-        chat_room = await (await createChatRoom(user1.id, user2.id)).json();
+        chat_room = await (await createChatRoom(token, user1.id, user2.id)).json();
     }
 
-    test('probar funcionalidad', async () => {
-        await tearUp();
-
-        /*
-        console.log(chat_room);
-        render(<Router>
-            <ChatComponent roomId={chat_room.id} roomName={user2.username} roomMate={user2.id} />
-        </Router>);
-
+    function checkPage() {
         expect(screen.getByText('Enviar')).toBeInTheDocument();
         expect(screen.findByPlaceholderText('Escribe un mensaje...'));
         expect(screen.getByText('Volver')).toBeInTheDocument();
         expect(screen.getByPlaceholderText('Escribe un mensaje...')).toBeInTheDocument();
         expect(screen.getByRole('button', {name: /enviar/i})).toBeInTheDocument();
+    }
+
+    function writeMessage(message) {
+        fireEvent.change(screen.getByPlaceholderText('Escribe un mensaje...'), {target: {value: message}});
+    }
+
+    function sendMessage() {
+        let botton = screen.getByText('Enviar');
+        botton.click();
+    }
+
+    async function checkMessage(message) {
+        let messages;
+        await act(async () => {
+            messages = await (await getMessages(token, chat_room.chatroomID)).json();
+        });
+
+        expect(messages.some(msg => msg.content === 'Hello, World!¡Hola, Mundo!123345')).toBe(true);
+    }
+
+    test('Renderiza', async () => {
+        await tearUp();
+        render(<Router>
+            <ChatPage/>
+        </Router>);
+    });
+
+    test('Comprueba estado de la página', async () => {
+        await tearUp();
+        render(
+            <Router>
+                <ChatComponent roomId={chat_room.chatroomID} roomName={user2.username} roomMate={user2.id}/>
+            </Router>
+        );
+        checkPage();
+    });
+
+
+    test(`Enviar mensaje normal `, async () => {
+        await tearUp();
+        render(
+            <Router>
+                <ChatComponent roomId={chat_room.chatroomID} roomName={user2.username} roomMate={user2.id}/>
+            </Router>
+        );
 
         // Simular la escritura de un mensaje en el input
-        fireEvent.change(screen.getByPlaceholderText('Escribe un mensaje...'), {target: {value: 'Hello, World!'}});
+        writeMessage('Hello, World!¡Hola, Mundo!123345');
 
         // Simular el clic en el botón de enviar
-        fireEvent.click(screen.getByText('Enviar'));
+        sendMessage();
 
-        // Verificar que el mensaje se haya enviado
-        waitFor(() => screen.getByText('Hello, World!'));
-
-
-        expect(screen.getByText('Hello, World!')).toBeInTheDocument();
-
-         */
-    })
-});
-
-
-async function getToken(name, password) {
-    let response = await fetch(`${backend}/users/login/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({username: name, password: password}),
+        // Esperar a que aparezca el mensaje enviado
+        await checkMessage('Hello, World!¡Hola, Mundo!123345');
     });
-    console.log(response);
-    return await response.json();
-}
+
+
+    const code_messages = [
+        'def hello_world():\n    print("Hello, World!")\n', // Código en Python
+        'public static void main(String[] args) {\n    System.out.println("Hello, World!");\n}', // Código en Java
+        'function helloWorld() {\n    console.log("Hello, World!");\n}', // Código en JavaScript
+        '<script>alert("Error");</script>', // Script malicioso
+        'SELECT * FROM users;', // Consulta SQL
+        'DROP TABLE users;', // Consulta SQL maliciosa
+    ]
+    code_messages.forEach((message, index) => {
+        test(`Enviar mensaje de código "${message}" (${index + 1}/${code_messages.length})`, async () => {
+            await tearUp();
+            render(
+                <Router>
+                    <ChatComponent roomId={chat_room.chatroomID} roomName={user2.username} roomMate={user2.id}/>
+                </Router>
+            );
+
+            // Simular la escritura de un mensaje en el input
+            writeMessage(message);
+
+            // Simular el clic en el botón de enviar
+            sendMessage();
+
+            // Esperar a que aparezca el mensaje enviado
+            checkMessage(message);
+        });
+    })
+
+
+    test(`Enviar mensaje con caracteres especiales`, async () => {
+        await tearUp();
+        render(
+            <Router>
+                <ChatComponent roomId={chat_room.chatroomID} roomName={user2.username} roomMate={user2.id}/>
+            </Router>
+        );
+
+        // Simular la escritura de un mensaje en el input
+        writeMessage('!@#$%^&*()_+{}|:"<>?`-=[]\;\',.😊🚀🌟🍕🍔🍟🌮🍿🥤');
+
+        // Simular el clic en el botón de enviar
+        sendMessage();
+
+        // Esperar a que aparezca el mensaje enviado
+        checkMessage('!@#$%^&*()_+{}|:"<>?`-=[]\;\',.😊🚀🌟🍕🍔🍟🌮🍿🥤');
+    });
+
+
+    test(`Enviar mensaje largo `, async () => {
+        await tearUp();
+        render(
+            <Router>
+                <ChatComponent roomId={chat_room.chatroomID} roomName={user2.username} roomMate={user2.id}/>
+            </Router>
+        );
+
+        // Simular la escritura de un mensaje en el input
+        writeMessage('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla lacinia risus, ac convallis ex ultrices in. Nullam ut ex quis sapien tincidunt rhoncus id at ligula. Nam volutpat nulla sed nibh malesuada vehicula. Sed id arcu at libero lacinia ultricies sit amet id nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer et tortor sem. Ut vestibulum felis nisi, vel efficitur leo suscipit ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla lacinia risus, ac convallis ex ultrices in. Nullam ut ex quis sapien tincidunt rhoncus id at ligula. Nam volut pat nulla sed nibh malesuada vehicula. Sed id arcu at libero lacinia ultricies sit amet id nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer et tortor sem. Ut vestibulum felis nisi, vel efficitur leo suscipit ac.');
+
+        // Simular el clic en el botón de enviar
+        sendMessage();
+
+        // Esperar a que aparezca el mensaje enviado
+        checkMessage('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla lacinia risus, ac convallis ex ultrices in. Nullam ut ex quis sapien tincidunt rhoncus id at ligula. Nam volutpat nulla sed nibh malesuada vehicula. Sed id arcu at libero lacinia ultricies sit amet id nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer et tortor sem. Ut vestibulum felis nisi, vel efficitur leo suscipit ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla lacinia risus, ac convallis ex ultrices in. Nullam ut ex quis sapien tincidunt rhoncus id at ligula. Nam volut pat nulla sed nibh malesuada vehicula. Sed id arcu at libero lacinia ultricies sit amet id nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer et tortor sem. Ut vestibulum felis nisi, vel efficitur leo suscipit ac.');
+    });
+});
