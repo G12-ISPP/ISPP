2547298ca30aee227da85b9f35f6006da359546f
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-16 13:50:30 +0100

    refactor: Deleted Tasks

--- frontend/src/api/tasks.api.js
+++
@@ -1,5 +0,0 @@
-export const getAllTasks = () => {
-  return fetch("http://localhost:8000/tasks/api/v1/tasks/").then((response) =>
-    response.json()
-  );
-};
--- frontend/src/pages/TaskFormPage.jsx
+++
@@ -1,3 +0,0 @@
-export function TaskFormPage() {
-  return <div>TaskFormPage page</div>;
-}
