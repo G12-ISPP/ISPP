50e063fba38a805970cb86392379ee7e1aade892
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-04-07 19:01:10 +0200

    fix: Other test now works

--- frontend/src/tests/Logout.test.jsx
+++ frontend/src/tests/Logout.test.jsx
@@ -1,25 +1,45 @@
-import { test } from 'vitest'
-import { render, screen } from "@testing-library/react";
-import MainPage from '../pages/MainPage'
-import '@testing-library/jest-dom'
+import {test} from 'vitest';
+import {render, screen, fireEvent} from "@testing-library/react";
+import MainPage from '../pages/MainPage';
+import '@testing-library/jest-dom';
+import {Navigation} from "../components/Navigation.jsx";
+import {AuthProvider} from "../context/AuthContext.jsx";
+import {MemoryRouter} from "react-router-dom";
 
-test('Cerrar sesión elimina token del localStorage', () => {
-    // Establecer el estado isLoggedIn a true
-    localStorage.setItem('token', 'fakeToken');
-    expect(localStorage.getItem('token')).not.toBeNull();
-    
-    // Renderizar el componente Header
-    render(<MainPage />);
-    setTimeout(() => {
+const messages = {
+    localStorageTokenKey: 'token',
+    localStorageTokenValue: 'user',
+    logoutButtonText: 'Cerrar sesión'
+};
+
+describe('Test para cerrar sesión', () => {
+    test('Cerrar sesión elimina token del localStorage', async () => {
+        // Establecer el estado isLoggedIn a true
+        localStorage.setItem(messages.localStorageTokenKey, messages.localStorageTokenValue);
+        expect(localStorage.getItem(messages.localStorageTokenKey)).not.toBeNull();
+
+        // Renderizar el componente MainPage
+        render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <Navigation/>
+                </AuthProvider>
+            </MemoryRouter>
+        );
+
+        // Esperar a que el componente se renderice completamente
+        await screen.findByText(messages.logoutButtonText);
+
         // Encontrar el botón "Cerrar sesión"
-    const closeButton = screen.getByText('Cerrar Sesión');
+        const closeButton = screen.getByText(messages.logoutButtonText);
 
-    // Simular un clic en el botón "Cerrar sesión"
-    fireEvent.click(closeButton);
+        // Simular un clic en el botón "Cerrar sesión"
+        fireEvent.click(closeButton);
 
-    // Verificar que no existe un token en el localStorage
-    expect(localStorage.getItem('token')).toBeNull();
-    }, 2000);
-    
-    
+        // Verificar que no existe un token en el localStorage después de un tiempo de espera
+        setTimeout(() => {
+            expect(localStorage.getItem(messages.localStorageTokenKey)).toBeNull();
+        }, 2000);
+    });
 });
+
--- frontend/src/tests/ProductDetail.test.jsx
+++ frontend/src/tests/ProductDetail.test.jsx
@@ -1,202 +1,221 @@
 import ProductDetail from '../components/Product';
-import { render, waitFor, screen } from "@testing-library/react";
-import { expect } from "chai";
-import { vi } from 'vitest';
+import {render, waitFor, screen} from "@testing-library/react";
+import {expect} from "chai";
+import {vi} from 'vitest';
+import {getUser, login} from "../api/users.api.jsx";
+import {saveProduct} from "../api/products.api.jsx";
+import {AuthProvider} from "../context/AuthContext.jsx";
+import {MemoryRouter} from "react-router-dom";
 
-describe('OK Product Details', () => {
-    test('Normal', async () => {
-        /* ARRANGE */
-        const seller = {
-            first_name: "Nicolás",
-            last_name: "Irizo Ramos"
-        };
+let messages = {
+    token: {
+        user: 'test1Frontend',
+        password: 'test1Frontend'
+    },
+    labels: {
+        title: 'Detalles del producto'
+    }
+}
 
-        mockFetch(seller);
+describe('Test products details', () => {
 
-        const product = {
-            description: "Diseño para impresión 3D de una gallina. Está pensado para la diversión y el juego de los más pequeños. Los colores son los mostrados en la foto. Se puede ajustar al tamaño que se desee.",
-            id: 9,
-            imageRoute: "design_gallina.jpg",
-            image_url: null,
-            name: "Diseño gallina",
-            price: "3.00",
-            product_type: "D",
-            seller: 10,
-            stock_quantity: 1
-        };
+    const jsdomAlert = window.alert;
+    window.alert = () => {
+    };
+    let token = undefined;
+    let user = undefined;
 
-        mockFetch(product);
+    beforeEach(async () => {
+        localStorage.removeItem('token')
 
-        /* ACT */
-        const { container } = render(<ProductDetail />);
+        token = (await (await login(messages.token.user, messages.token.user)).json()).token;
 
-        await waitFor(() => {expect(screen.getByText("Detalles del producto")).to.exist;});
-
-        /* ASSERT */
-        assertOKProductDetails(product, seller, container);
+        user = await (await getUser(12)).json();
+        localStorage.setItem('token', token);
     });
 
-    test('Product with Oriental Characters', async () => {
-        /* ARRANGE */
-        const seller = {
-            first_name: "東方東方",
-            last_name: "言語"
-        };
+    function loadRender() {
+        return render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <ProductDetail/>
+                </AuthProvider>
+            </MemoryRouter>
+        );
+    }
 
-        mockFetch(seller);
 
-        const product = {
-            description: "3D印刷のデザイン。子供たちの遊びと楽しみのために考案されました。写真に表示されている色です。サイズを調整できます。",
-            id: 9,
-            imageRoute: "oriental_design.jpg",
-            image_url: null,
-            name: "東方デザイン",
-            price: "10.00",
-            product_type: "O",
-            seller: 10,
-            stock_quantity: 5
-        };
+    describe('OK Product Details', () => {
 
-        mockFetch(product);
+        test('Normal', async () => {
+            /* ARRANGE */
+            const product = {
+                description: "Diseño para impresión 3D de una gallina. Está pensado para la diversión y el juego de los más pequeños. Los colores son los mostrados en la foto. Se puede ajustar al tamaño que se desee.",
+                id: 9,
+                imageRoute: "design_gallina.jpg",
+                image_url: null,
+                name: "Diseño gallina",
+                price: "3.00",
+                product_type: "D",
+                seller: user.id,
+                stock_quantity: 20
+            };
 
-        /* ACT */
-        const { container } = render(<ProductDetail />);
+            mockFetch(product);
 
-        await waitFor(() => {expect(screen.getByText("Detalles del producto")).to.exist;});
+            /* ACT */
+            const {container} = loadRender();
 
-        /* ASSERT */
-        assertOKProductDetails(product, seller, container);
+            await waitFor(() => {
+                expect(screen.getByText(messages.labels.title)).to.exist;
+            });
+
+            /* ASSERT */
+            assertOKProductDetails(product, user, container);
+        });
+
+        test('Product with Oriental Characters', async () => {
+            /* ARRANGE */
+            const product = {
+                description: "3D印刷のデザイン。子供たちの遊びと楽しみのために考案されました。写真に表示されている色です。サイズを調整できます。",
+                id: 9,
+                imageRoute: "oriental_design.jpg",
+                image_url: null,
+                name: "東方デザイン",
+                price: "10.00",
+                product_type: "O",
+                seller: user.id,
+                stock_quantity: 5
+            };
+
+            await saveProduct(product);
+
+            mockFetch(product);
+
+            /* ACT */
+            const {container} = loadRender();
+
+            await waitFor(() => {
+                expect(screen.getByText(messages.labels.title)).to.exist;
+            });
+
+            /* ASSERT */
+            assertOKProductDetails(product, user, container);
+        });
+
+        test('Product with Extreme Price', async () => {
+            /* ARRANGE */
+            const product = {
+                description: "Product with Extreme Price",
+                id: 9,
+                imageRoute: "extreme_price.jpg",
+                image_url: null,
+                name: "High Price Product",
+                price: "999999.99", // Extremely high price
+                product_type: "E",
+                seller: user.id,
+                stock_quantity: 20
+            };
+
+            await saveProduct(product);
+
+            mockFetch(product);
+
+            /* ACT */
+            const {container} = loadRender();
+
+            await waitFor(() => {
+                expect(screen.getByText(messages.labels.title)).to.exist;
+            });
+
+            /* ASSERT */
+            assertOKProductDetails(product, user, container);
+        });
     });
+    describe("Injection Product Details", () => {
+        test('Product with JavaScript Injection', async () => {
+            /* ARRANGE */
+            const product = {
+                description: "<script>alert('JavaScript Injection');</script>",
+                id: "9",
+                imageRoute: "javascript_injection.jpg",
+                image_url: "null",
+                name: "Product with JS Injection",
+                price: "15.00",
+                product_type: "J",
+                seller: "10",
+                stock_quantity: "3"
+            };
 
-    test('Product with Extreme Price', async () => {
-        /* ARRANGE */
-        const seller = {
-            first_name: "Nicolás",
-            last_name: "Irizo Ramos"
-        };
+            mockFetch(product);
 
-        mockFetch(seller);
+            /* ACT */
+            const {container} = loadRender();
 
-        const product = {
-            description: "Product with Extreme Price",
-            id: 9,
-            imageRoute: "extreme_price.jpg",
-            image_url: null,
-            name: "High Price Product",
-            price: "999999.99", // Extremely high price
-            product_type: "E",
-            seller: 10,
-            stock_quantity: 1
-        };
+            await waitFor(() => {
+                expect(screen.getByText("Detalles del producto")).to.exist;
+            });
 
-        mockFetch(product);
+            /* ASSERT */
+            assertInjectionProductDetails(product, user, container);
+        });
 
-        /* ACT */
-        const { container } = render(<ProductDetail />);
+        test('Product with HTML Injection', async () => {
+            /* ARRANGE */
+            const product = {
+                description: "<div style='color:red;'>HTML Injection</div>",
+                id: "9",
+                imageRoute: "html_injection.jpg",
+                image_url: "null",
+                name: "Product with HTML Injection",
+                price: "20.00",
+                product_type: "H",
+                seller: user.id,
+                stock_quantity: "4"
+            };
 
-        await waitFor(() => {expect(screen.getByText("Detalles del producto")).to.exist;});
+            mockFetch(product);
 
-        /* ASSERT */
-        assertOKProductDetails(product, seller, container);
+            /* ACT */
+            const {container} = loadRender();
+
+            await waitFor(() => {
+                expect(screen.getByText(messages.labels.title)).to.exist;
+            });
+
+            /* ASSERT */
+            assertInjectionProductDetails(product, user, container);
+        });
+
+        test('Product with SQL Injection', async () => {
+            /* ARRANGE */
+            const product = {
+                description: "Product with SQL Injection DESCRIPTION; DROP TABLE Products;",
+                id: "9; DROP TABLE Products;",
+                imageRoute: "sql_injection.jpg",
+                image_url: "null",
+                name: "Product with SQL Injection NAME; DROP TABLE Products;",
+                price: "25.00",
+                product_type: "S",
+                seller: user.id,
+                stock_quantity: "6"
+            };
+
+            mockFetch(product);
+
+            /* ACT */
+            const {container} = loadRender();
+
+            await waitFor(() => {
+                expect(screen.getByText(messages.labels.title)).to.exist;
+            });
+
+            /* ASSERT */
+            assertInjectionProductDetails(product, user, container);
+        });
     });
 });
-describe("Injection Product Details", () => {
-    test('Product with JavaScript Injection', async () => {
-        /* ARRANGE */
-        const seller = {
-            first_name: "Nicolás",
-            last_name: "Irizo Ramos"
-        };
 
-        mockFetch(seller);
-
-        const product = {
-            description: "<script>alert('JavaScript Injection');</script>",
-            id: "9",
-            imageRoute: "javascript_injection.jpg",
-            image_url: "null",
-            name: "Product with JS Injection",
-            price: "15.00",
-            product_type: "J",
-            seller: "10",
-            stock_quantity: "3"
-        };
-
-        mockFetch(product);
-
-        /* ACT */
-        const { container } = render(<ProductDetail />);
-
-        await waitFor(() => {expect(screen.getByText("Detalles del producto")).to.exist;});
-
-        /* ASSERT */
-        assertInjectionProductDetails(product, seller, container);
-    });
-
-    test('Product with HTML Injection', async () => {
-        /* ARRANGE */
-        const seller = {
-            first_name: "Nicolás",
-            last_name: "Irizo Ramos"
-        };
-
-        mockFetch(seller);
-
-        const product = {
-            description: "<div style='color:red;'>HTML Injection</div>",
-            id: "9",
-            imageRoute: "html_injection.jpg",
-            image_url: "null",
-            name: "Product with HTML Injection",
-            price: "20.00",
-            product_type: "H",
-            seller: "10",
-            stock_quantity: "4"
-        };
-
-        mockFetch(product);
-
-        /* ACT */
-        const { container } = render(<ProductDetail />);
-
-        await waitFor(() => {expect(screen.getByText("Detalles del producto")).to.exist;});
-
-        /* ASSERT */
-        assertInjectionProductDetails(product, seller, container);
-    });
-
-    test('Product with SQL Injection', async () => {
-        /* ARRANGE */
-        const seller = {
-            first_name: "Nicolás",
-            last_name: "Irizo Ramos"
-        };
-
-        mockFetch(seller);
-
-        const product = {
-            description: "Product with SQL Injection DESCRIPTION; DROP TABLE Products;",
-            id: "9; DROP TABLE Products;",
-            imageRoute: "sql_injection.jpg",
-            image_url: "null",
-            name: "Product with SQL Injection NAME; DROP TABLE Products;",
-            price: "25.00",
-            product_type: "S",
-            seller: "10",
-            stock_quantity: "6"
-        };
-
-        mockFetch(product);
-
-        /* ACT */
-        const { container } = render(<ProductDetail />);
-
-        await waitFor(() => {expect(screen.getByText("Detalles del producto")).to.exist;});
-
-        /* ASSERT */
-        assertInjectionProductDetails(product, seller, container);
-    });
-});
 
 // Auxiliar functions
 function assertOKProductDetails(product, seller, container) {
