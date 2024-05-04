abfc23e2d94eb06385ec88dad62ea163bf302272
davcorrom <davcorrom@alum.us.es>
2024-05-04 17:29:58 +0200

    Feat: Página de plan confirmado

--- frontend/src/components/BuyPlan/ConfirmPlan.jsx
+++ frontend/src/components/BuyPlan/ConfirmPlan.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import './BuyPlan.css';
+import logo from '../../assets/logo_with_background.png';
 import PageTitle from '../PageTitle/PageTitle';
 
-export default class CancelPlan extends React.Component{
-render(){
-    return(
-        <>
+const CancelPlan = () => {
+
+    const onButtonClick = (path) => {
+        window.location.href = path;
+    };
+
+    return (
+        <div className="plan-confirmed-page">
             <PageTitle title="Plan confirmado" />
-            <div className="custom-design-details">
-                <h1>Pago realizado correctamente, disfruta de tus nuevas ventajas</h1>
+            <div className="plan-confirmed-container">
+                <div className="plan-confirmed-contents-wrapper">
+                    <div className="logo-container">
+                        <img src={logo} alt="logo" className="logo" onClick={() => onButtonClick('/')} />
+                    </div>
+                    <div className="plan-confirmed-text">
+                        <h3 className="plan-confirmed-text-primary">Pago realizado correctamente, disfruta de tus nuevas ventajas.</h3>
+                        <p className="plan-confirmed-text-secondary">El equipo de SHAR3D</p>
+                    </div>
+                </div>
             </div>
-        </>
-    )
-}
-}
+        </div>
+    );
+};
+
+export default CancelPlan;
